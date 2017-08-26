c1075505a88dea07c8ad57b84940e635c7ba478a
Almaz Murzin <almaz.murzin.gdc@ts.fujitsu.com>
2017-08-26 16:17:18 +0300

    initial commit

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+            }
+            return View(results);
+        }
+    }
+}
